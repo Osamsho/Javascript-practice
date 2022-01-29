399cdb05332e87b5b30cb3a7724282bdece0f490
Tajuddin-samsul <samsultaj22@gmail.com>
2022-01-30 00:30:31 +0600

    Object

--- function.js
+++ function.js
@@ -1,16 +1,20 @@
-// Function - block of code
+// Function is a block of code....
+// Example-1
+
         // function startFan() {
-        //     console.log('Go and touch'); //commands
+        //     console.log('Go to the switch'); //curly bracket contains commands to perform.
         //     console.log('tap switch');
         // }
 
         // startFan();
         
 
-// FUNCTION PARAMETER .....
+// Example-2..  FUNCTION PARAMETER .....
 
-function mul(taka1, taka2) {
-   console.log('multiplication:', taka1 * taka2); // comma before taka- caution...
-}
+                // function mul(p1, p2) {
+                //    console.log('multiplication:', p1 * p2); // CAUTION- COMMA BEFORE PARAMETER...
+                // }
 
-mul(5,4);
+                // mul(5,4);
+
+// Example-3...
